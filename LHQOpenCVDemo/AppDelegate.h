7ab9172a0043badaa99391ef6b3bc55bc0545643
WaterWorld <lin@waterwolddeimac.lan>
2019-04-24 18:21:40 +0800

    Initial Commit

---
+++ LHQOpenCVDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LHQOpenCVDemo
+//
+//  Created by WaterWorld on 2019/4/24.
+//  Copyright © 2019年 linhuaqin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LHQOpenCVDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LHQOpenCVDemo
+//
+//  Created by WaterWorld on 2019/4/24.
+//  Copyright © 2019年 linhuaqin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
